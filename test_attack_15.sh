91b58534f9da36c2f76e7da55fbaccb6893d45c6
cx2 <cx2@illinois.edu>
2019-03-04 11:35:24 -0600

    attacking median and krum

--- test_attack_15.sh
+++ test_attack_15.sh
@@ -8,13 +8,13 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 # stdbuf -o 0 python mxnet_cnn_cifar10_impl.py --gpu 0 --nrepeats 2 --nepochs 200 --lr 0.05 --batch_size 50 --nworkers 25 --nbyz 11 --byz_type signflip --byz_factor 1 --byz_start 100 --aggregation krum | tee /home/nfs/cx2/src/byz/zeno/results/test_zeno_cifar10_signflip_zeno_8_bs_1.txt
 
-gpu=1
+gpu=3
 aggr="krum"
 nrepeats=2
 
 logdir=/home/nfs/cx2/src/byz_attack/results
 
-for byzstart in 10
+for byzstart in 50
 do
     for byzfactor in 1 0.5
     do
